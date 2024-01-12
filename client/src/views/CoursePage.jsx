d12fff29680536065a0db2632e2a09f54e36c039
Ritesh Patil <ritessshhh7@gmail.com>
2024-01-12 23:43:52 +0530

    Minor ui change

--- client/src/views/CoursePage.jsx
+++ client/src/views/CoursePage.jsx
@@ -13,7 +13,7 @@ const CoursePage = () => {
   const course = paras.get("course");
 
   return (
-    <div className="course-page text-gray-700 w-screen py-4 md:px-[5rem] min-h-[87vh] bg-violet-200">
+    <div className="course-page text-gray-700 py-4 md:px-[5rem] min-h-[87vh] bg-violet-200">
       <h1 className="text-lg md:text-3xl text-center border-b-black">{`${branch} > ${semester} > ${course}`}</h1>
       <div className="category-container w-[95vw] max-w-[58rem] mt-8 mx-auto">
         {categories.map((c, i) => (
